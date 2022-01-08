8a5109991711d6ca5ed99efa2f01bcd423cb4147
Daniel Martin <xadlien@gmail.com>
2022-01-08 18:15:41 -0500

    Same

--- update-ansiblehosts.sh
+++ update-ansiblehosts.sh
@@ -1,2 +1,2 @@
 ip_addr=`sed -rn "s/.*([0-9]{3}\.[0-9]+\.[0-9]+\.[0-9]+).*/\1/p" terraform/network/terraform.tfstate | head -n1`
-echo -e "[wireguard]\n${ip_addr}" >> ansible/ansible_hosts_automated
+echo -e "[wireguard]\n${ip_addr}" > ansible/ansible_hosts_automated
